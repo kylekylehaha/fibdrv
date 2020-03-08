6c9052c46875350637747d63d973f03f56e70e7f
kylekylehaha <kyle870216@gmail.com>
2020-03-08 20:57:13 +0800

    Add calculate time mechanism

--- client.c
+++ client.c
@@ -3,17 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
+unsigned int diff_in_ns(struct timespec t1, struct timespec t2)
+{
+    struct timespec result;
+    if ((t2.tv_nsec - t1.tv_nsec) < 0) {
+        result.tv_sec = t2.tv_sec - t1.tv_sec - 1;
+        result.tv_nsec = 1000000000 + t2.tv_nsec - t1.tv_nsec;
+    } else {
+        result.tv_sec = t2.tv_sec - t1.tv_sec;
+        result.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (unsigned int) result.tv_sec * 1000000000 + result.tv_nsec;
+}
+
 int main()
 {
+    FILE *fp = fopen("time.txt", "wb+");
     long long sz;
 
     char buf[1];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
+    int offset = 10; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -28,22 +43,28 @@ int main()
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
+        struct timespec start, end;
+        clock_gettime(CLOCK_REALTIME, &start);
         sz = read(fd, buf, 1);
+        clock_gettime(CLOCK_REALTIME, &end);
+        fprintf(fp, "%d %u\n", i, diff_in_ns(start, end));
+
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
                "%lld.\n",
                i, sz);
     }
-
-    for (int i = offset; i >= 0; i--) {
-        lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
-    }
-
+    /*
+        for (int i = offset; i >= 0; i--) {
+            lseek(fd, i, SEEK_SET);
+            sz = read(fd, buf, 1);
+            printf("Reading from " FIB_DEV
+                   " at offset %d, returned the sequence "
+                   "%lld.\n",
+                   i, sz);
+        }
+    */
+    fclose(fp);
     close(fd);
     return 0;
 }
